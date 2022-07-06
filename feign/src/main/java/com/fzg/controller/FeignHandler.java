f7ba3e4ed3484e918e1196fcc8de590e4df31f3d
fzg1987 <beginer@yeah.net>
2022-07-07 00:35:37 +0800

    声明式接口调用Feign

---
+++ feign/src/main/java/com/fzg/controller/FeignHandler.java
@@ -0,0 +1,41 @@
+package com.fzg.controller;
+
+import com.fzg.entity.Student;
+import com.fzg.feign.FeignProviderClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/feign")
+public class FeignHandler {
+    @Autowired
+    private FeignProviderClient feignProviderClient;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        return feignProviderClient.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findById(@PathVariable("id") Integer id){
+        return feignProviderClient.findById(id);
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Student student){
+        feignProviderClient.save(student);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Student student){
+        feignProviderClient.update(student);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") Integer id){
+        feignProviderClient.deleteById(id);
+    }
+
+}
